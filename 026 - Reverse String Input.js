b074b999ec98cd50636cc5cb5cc3308a25d8cb38
Jordy Veenstra <67433684+jiyorude@users.noreply.github.com>
2022-12-31 00:43:24 +0100

    String input Reverse Function

---
+++ 026 - Reverse String Input.js
@@ -0,0 +1,12 @@
+/*
+CHALLENGE:
+Write a function that reverses stringdata
+*/
+
+stringReverse = (str) => {
+  console.log(str.split("").reverse().join(""));
+};
+
+stringReverse("Hello, how are you doing?"); // ?gniod uoy era woh ,olleH
+stringReverse("I am on my way to work"); // krow ot yaw ym no ma I
+stringReverse("Are you wathing TV?"); //?VT gnihtaw uoy erA
