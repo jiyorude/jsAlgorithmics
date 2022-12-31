d19b2bac538de0901f03a624d50e1039e8dbac85
Jordy Veenstra <67433684+jiyorude@users.noreply.github.com>
2022-12-31 21:51:01 +0100

    Looping a Triangle

---
+++ 030 - Looping a Triangle.js
@@ -0,0 +1,24 @@
+/*
+CHALLENGE:
+Found in Eloquent JavaScript:
+Write a loop that outputs a hash-triangle
+*/
+
+let hash = "";
+
+for (let x = 0; x <= 6; x++) {
+  hash += "#";
+  console.log(hash);
+}
+
+/*
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
