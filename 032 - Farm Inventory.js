b5eb1a098245cf7d07dcd80c49bdf9ff6b3224bf
Jordy Veenstra <67433684+jiyorude@users.noreply.github.com>
2023-01-01 14:46:57 +0100

    Farm Inventory

--- 032 - Farm Inventory.js
+++ 032 - Farm Inventory.js
@@ -4,20 +4,20 @@ Create a farm inventory app
 as seen in Eloquent JavaScript
 */
 
-zeroPaddedLabel = (num, label) => {
+zeroPaddedLabel = (num, width) => {
   let numString = String(num);
-  while (numString.length < 3) {
+  while (numString.length < width) {
     numString = "0" + numString;
   }
-  console.log(`${numString} ${label}`);
+  return numString;
 };
 
 farmInventory = (cows, chickens, pigs, sheep, horses) => {
-  zeroPaddedLabel(cows, "Cows");
-  zeroPaddedLabel(chickens, "Chickens");
-  zeroPaddedLabel(pigs, "Pigs");
-  zeroPaddedLabel(sheep, "Sheep");
-  zeroPaddedLabel(horses, "Horses");
+  console.log(zeroPaddedLabel(cows, 3) + " Cows");
+  console.log(zeroPaddedLabel(chickens, 3) + " Chickens");
+  console.log(zeroPaddedLabel(pigs, 3) + " Pigs");
+  console.log(zeroPaddedLabel(sheep, 3) + " Sheep");
+  console.log(zeroPaddedLabel(horses, 3) + " Horses");
 };
 
 farmInventory(12, 55, 9, 10, 3);
