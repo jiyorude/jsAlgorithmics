5db5ae3ba96970cbfd097727070958429ef8b08b
Jordy Veenstra <67433684+jiyorude@users.noreply.github.com>
2023-01-03 16:17:44 +0100

    Login system with if-else

---
+++ 040 - Login System with if:else.js
@@ -0,0 +1,16 @@
+let user = prompt("Who's there?");
+
+if (user === "Admin") {
+  let password = prompt("Password?");
+  if (password === "TheMaster") {
+    alert("Welcome!");
+  } else if (password === "" || password === null || password === undefined) {
+    alert("Cancelled!");
+  } else {
+    alert("Wrong Password!");
+  }
+} else if (user === null || user === "" || user === undefined) {
+  alert("Cancelled!");
+} else {
+  alert("I don't know you.");
+}
