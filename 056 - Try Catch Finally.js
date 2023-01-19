1411ac734b717481d953ec4a77df33254df8f220
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-01-19 16:53:14 +0100

    commit of final challenges
    uploading empty final challenges

---
+++ 056 - Try Catch Finally.js
@@ -0,0 +1,11 @@
+/*
+CHALLENGE:
+Explain 'try', 'catch' and 'finally'
+as seen on freeCodeCamp
+*/
+
+// try... catch allows us to try code blocks and catch errors.
+// In "try", we specify the function or code we'd like to run.
+// In Catch, we specify what JavaScript should return if the code were to fail. Instead of dying, the code will return this instead.
+// In finally, we should put a correct code execution: a guarantee if you will that your code will execute.
+// Throw allows you to create custom errors.
